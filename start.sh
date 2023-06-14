f75a8f02a440c4e3db084d8a6c243c904aabd1d3
Stanislav Hetzel <stanislav.hetzel@ffuf.de>
2023-06-14 10:29:05 +0200

    Better start script

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
-streamlit run app.py
+#!/bin/bash
+port=$1
+
+if [[ ! $port =~ ^[0-9]+$ ]]; then
+  echo "Invalid port. Please provide a valid numeric port number."
+  exit 1
+fi
+
+streamlit run app.py --server.port $port
